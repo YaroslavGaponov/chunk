43a3d6d54961cdce2b2e17355e13db4ae0bf8f1c
Yaroslav Gaponov <yaroslav.gaponov@avid.com>
2015-11-30 07:01:19 -0500

    queue fixed

--- src/queue.c
+++ src/queue.c
@@ -3,35 +3,120 @@
 #include "queue.h"
 
 int queue_init(QUEUE* queue)
-{   
-    queue->length = 0;
+{
+    queue->head = NULL;
+    queue->tail = NULL;
     
     return 0;
 }
 
 int queue_clear(QUEUE* queue)
 {
+    QNODE* curr;
+    QNODE* pred;
     
+    curr = queue->head;
+    while (curr != NULL) {
+        pred = curr;
+        curr = curr->next;
+        free(pred->value);
+        free(pred);
+    }
+    
+    queue->head = NULL;
+    queue->tail = NULL;
+
     return 0;
+}
+
+int queue_is_empty(QUEUE* queue) {
+    return queue->head == NULL;
 }
 
 int queue_insert(QUEUE* queue, char* value)
 {
+    QNODE* node;
+    
+    node = malloc(sizeof(QNODE));
+    node->value = strdup(value);
+    node->next = NULL;
+    
+    if (queue->head == NULL) {
+        queue->head = queue->tail = node;
+    } else {
+        queue->tail->next = node;
+        queue->tail = queue->tail->next;
+    }
     
     return 0;
 }
 
 int queue_remove(QUEUE* queue, char* value)
 {
-   
-   return 1; 
+    QNODE* curr;
+    QNODE* pred;
+    
+    curr = queue->head;
+    pred = NULL;    
+    while (curr != NULL) {
+        if (strcmp(curr->value, value) == 0) {
+            if (queue->tail == curr) {
+                if (curr->next == NULL) {
+                    queue->tail = pred;
+                } else {
+                    queue->tail = curr->next;
+                }
+            }
+            if (pred == NULL) {
+                queue->head = curr->next;
+            } else {
+                pred->next = curr->next;
+            }
+            free(curr->value);
+            free(curr);
+            return 0;
+        }
+        pred = curr;
+        curr = curr->next;
+    }
+
+    return -1;
 }
 
 char* queue_peek(QUEUE* queue)
 {
-    return NULL;
+    char* value;
+    QNODE* head;
+    
+    if (queue->head == NULL) {
+        return NULL;
+    }
+    
+    value = queue->head->value;
+    head = queue->head->next;    
+    free(queue->head);
+    queue->head = head;
+    if (queue->head == NULL) {
+        queue->tail = NULL;
+    }
+    
+    return value;
+    
 }
 
 void queue_dispose(QUEUE* queue)
 {
+    QNODE* curr;
+    QNODE* pred;
+    
+    curr = queue->head;
+    while (curr != NULL) {
+        pred = curr;
+        curr = curr->next;
+        free(pred->value);
+        free(pred);
+    }
+    
+    queue->head = NULL;
+    queue->tail = NULL;
 }
--- src/queue.h
+++ src/queue.h
@@ -3,13 +3,21 @@
 
 #include <stdlib.h>
 
+struct QNODE {
+    char *value;
+    struct QNODE* next; 
+};
+
+typedef struct QNODE QNODE;
 
 typedef struct QUEUE {
-    int length;
+    QNODE* head;
+    QNODE* tail;
 } QUEUE;
 
 int queue_init(QUEUE* queue);
-int queue_clear();
+int queue_clear(QUEUE* queue);
+int queue_is_empty(QUEUE* queue);
 int queue_insert(QUEUE* queue, char* value);
 int queue_remove(QUEUE* queue, char* value);
 char* queue_peek(QUEUE* queue);
